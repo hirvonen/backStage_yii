f581b593b4450d867983dacaca1e841631283c46
hirvonen <advancedanimal@gmail.com>
2015-09-06 12:47:41 +0800

    db

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,12 +5,7 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	'connectionString' => 'mysql:host=localhost;dbname=eyoungdb_150905',
-//	'connectionString' => 'mysql:host=121.41.104.220;dbname=eyoungdb',
-//	'connectionString' => 'mysql:host=10.168.241.67;dbname=eyoungdb',
-//	'connectionString' => 'mysql:host=rds0gna5y7u92a0uk0wa4.mysql.rds.aliyuncs.com;dbname=eyoungdb',
 	'emulatePrepare' => true,
-//	'username' => 'eyoungadmin',
-//	'password' => 'Cccc1111',
 	'username' => 'root',
 	'password' => 'root',
 	'charset' => 'utf8',
